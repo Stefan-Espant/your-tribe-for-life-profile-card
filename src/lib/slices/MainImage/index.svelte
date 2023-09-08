e30cd5ef8252eb37e518a9d690f65c73502c5292
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-09-08 11:15:14 +0200

    v1.1
    fixes

--- src/lib/slices/MainImage/index.svelte
+++ src/lib/slices/MainImage/index.svelte
@@ -5,16 +5,18 @@
 </script>
 
 <div data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	<img src={slice.primary.mainimage.url} alt={slice.primary.mainimage.alt} />
+	<img src={slice.primary.mainimage.url} alt={slice.primary.mainimage.alt} width={slice.primary.mainimage.dimensions.width} height={slice.primary.mainimage.dimensions.height} />
 </div>
-<!-- 
-<pre>
+
+<!-- <pre>
 	{JSON.stringify(slice, null, 3)}
 </pre> -->
 
 <style>
 	img {
 		width: 12em;
+		height: auto;
+		aspect-ratio: 1/1;
 		border-radius: 0.5em;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,6 @@
 	<section>
 		<SliceZone slices={data.slices} {components} />
 	</section>
-
 </main>
 
 <style>
@@ -17,16 +16,44 @@
 		width: 100%;
 		height: 100vh;
 		display: flex;
-        background-image: linear-gradient(135deg, #b51616 0% , #e55959 10% , #e55959 20%, #512b9e 20%, #512b9e 20%, #b18aff 60%, #203ea8 60%, #203ea8 60%, #5178ff 100%);
-        position: relative;
+		background-image: linear-gradient(
+			135deg,
+			#c30505 0%,
+			#ff5151 30%,
+			#512b9e 30%,
+			#512b9e 30%,
+			#b18aff 60%,
+			#203ea8 60%,
+			#5178ff 100%
+		);
+		position: relative;
 	}
 
 	section {
 		width: 20em;
+		height: fit-content;
 		margin: auto;
 		padding: 1em;
 		background-color: #f4f4f44a;
-        border-radius: 1em;
+		border-radius: 1em;
+		opacity: 0;
 		z-index: 1;
+		animation: fade-in 0.9s 1s cubic-bezier(0.075, 0.82, 0.165, 1) forwards;
+	}
+
+	@keyframes fade-in {
+		0% {
+			transform: translateY(1em);
+			opacity: 0;
+		}
+
+		66% {
+			transform: translateY(-0.33em);
+		}
+
+		100% {
+			transform: translateY(0em);
+			opacity: 1;
+		}
 	}
 </style>
